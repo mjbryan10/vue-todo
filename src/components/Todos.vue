f009618b47968ea92b461577a7dfab15100f1a3f
mjbryan <mjbyran10@gmail.com>
2020-05-01 13:52:27 +0200

    intial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Todos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
